618ef23da2dc1b3d6e6ae927274807a9e73c6ffe
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2020-12-01 17:50:27 +0100

    add protocol to host

--- src/test/scala/org/kibanaLoadTest/ingest/Main.scala
+++ src/test/scala/org/kibanaLoadTest/ingest/Main.scala
@@ -9,8 +9,7 @@ import org.kibanaLoadTest.helpers.Helper.{getLastReportPath, getTargetPath}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val host = System.getenv("HOST_FROM_VAULT")
-    println("host=" + host)
+    val host = "https://" + System.getenv("HOST_FROM_VAULT")
     val username = System.getenv("USER_FROM_VAULT")
     val password = System.getenv("PASS_FROM_VAULT")
 
