97e7ef92bc9ed4ee2e29cf3f4d16c34ff1d52ff4
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-01-18 15:22:59 +0100

    cleanup

--- src/test/scala/org/kibanaLoadTest/ingest/Main.scala
+++ src/test/scala/org/kibanaLoadTest/ingest/Main.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
 import org.kibanaLoadTest.ESConfiguration
-import org.kibanaLoadTest.helpers.{ESWrapper}
+import org.kibanaLoadTest.helpers.ESWrapper
 import org.kibanaLoadTest.helpers.Helper.{getLastReportPath, getTargetPath}
 
 object Main {
