1fcc92e66350ff2675eda04faa5d0933492ab6fd
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-01-26 18:39:28 +0100

    pause between scn

--- src/test/scala/org/kibanaLoadTest/simulation/CloudAtOnceJourney.scala
+++ src/test/scala/org/kibanaLoadTest/simulation/CloudAtOnceJourney.scala
@@ -24,28 +24,32 @@ class CloudAtOnceJourney extends BaseSimulation {
       TimeUnit.SECONDS
     )
 
-  val stepPause =
+  val scnPause =
     FiniteDuration(
-      5,
+      20,
       TimeUnit.SECONDS
     )
 
   val scnDiscover: ScenarioBuilder = scenario(scenarioName("discover"))
     .exec(loginStep.pause(loginPause))
-    .exec(Discover.doQuery(appConfig.baseUrl, defaultHeaders).pause(stepPause))
+    .exec(Discover.doQuery(appConfig.baseUrl, defaultHeaders))
 
   val scnDashboard: ScenarioBuilder = scenario(scenarioName("dashboard"))
     .exec(loginStep.pause(loginPause))
-    .exec(Dashboard.load(appConfig.baseUrl, defaultHeaders).pause(stepPause))
+    .exec(Dashboard.load(appConfig.baseUrl, defaultHeaders))
 
   val scnCanvas: ScenarioBuilder = scenario(scenarioName("canvas"))
     .exec(loginStep.pause(loginPause))
     .exec(Canvas.loadWorkpad(appConfig.baseUrl, defaultHeaders))
 
   setUp(
-    scnDiscover.inject(atOnceUsers(80)).protocols(httpProtocol),
-    scnDashboard.inject(atOnceUsers(80)).protocols(httpProtocol),
-    scnCanvas.inject(atOnceUsers(80)).protocols(httpProtocol)
-  ).maxDuration(simulationTimeout)
+    scnDiscover
+      .inject(atOnceUsers(80), nothingFor(scnPause))
+      .andThen(
+        scnDashboard
+          .inject(atOnceUsers(80), nothingFor(scnPause))
+          .andThen(scnCanvas.inject(atOnceUsers(80)))
+      )
+  ).protocols(httpProtocol).maxDuration(simulationTimeout)
 
 }
