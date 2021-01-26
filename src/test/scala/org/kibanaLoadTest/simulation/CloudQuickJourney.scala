b44ddfefefadf30653beb7068be8bd483af5f618
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-01-26 15:19:40 +0100

    [cloud scenario] at once 70u

---
+++ src/test/scala/org/kibanaLoadTest/simulation/CloudQuickJourney.scala
@@ -0,0 +1,31 @@
+package org.kibanaLoadTest.simulation
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import org.kibanaLoadTest.scenario.{Canvas, Dashboard, Discover, Login}
+
+class CloudQuickJourney extends BaseSimulation {
+  val scenarioName = s"Kibana cloud quick journey ${appConfig.buildVersion}"
+
+  val scn: ScenarioBuilder = scenario(scenarioName)
+    .exec(
+      Login
+        .doLogin(
+          appConfig.isSecurityEnabled,
+          appConfig.loginPayload,
+          appConfig.loginStatusCode
+        )
+        .pause(5)
+    )
+    .exec(Discover.doQuery(appConfig.baseUrl, defaultHeaders).pause(10))
+    .exec(Dashboard.load(appConfig.baseUrl, defaultHeaders).pause(10))
+    .exec(Canvas.loadWorkpad(appConfig.baseUrl, defaultHeaders))
+
+  setUp(
+    scn
+      .inject(
+        atOnceUsers(70) // 1
+      )
+      .protocols(httpProtocol)
+  ).maxDuration(10 * 60)
+}
