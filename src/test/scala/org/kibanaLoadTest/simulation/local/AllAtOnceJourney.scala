912307660fb50cb89e8f3e44342931abc5fbbc71
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-01-28 13:00:05 +0100

    run new script

---
+++ src/test/scala/org/kibanaLoadTest/simulation/local/AllAtOnceJourney.scala
@@ -0,0 +1,56 @@
+package org.kibanaLoadTest.simulation.local
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import org.kibanaLoadTest.scenario.{Canvas, Dashboard, Discover}
+import org.kibanaLoadTest.simulation.BaseSimulation
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
+
+class CloudAtOnceJourney extends BaseSimulation {
+  def scenarioName(module: String): String = {
+    s"Cloud  atOnce ${module} ${appConfig.buildVersion}"
+  }
+
+  val simulationTimeout =
+    FiniteDuration(
+      10,
+      TimeUnit.MINUTES
+    )
+
+  val loginPause =
+    FiniteDuration(
+      2,
+      TimeUnit.SECONDS
+    )
+
+  val scnPause =
+    FiniteDuration(
+      20,
+      TimeUnit.SECONDS
+    )
+
+  val scnDiscover: ScenarioBuilder = scenario(scenarioName("discover"))
+    .exec(loginStep.pause(loginPause))
+    .exec(Discover.doQuery(appConfig.baseUrl, defaultHeaders))
+
+  val scnDashboard: ScenarioBuilder = scenario(scenarioName("dashboard"))
+    .exec(loginStep.pause(loginPause))
+    .exec(Dashboard.load(appConfig.baseUrl, defaultHeaders))
+
+  val scnCanvas: ScenarioBuilder = scenario(scenarioName("canvas"))
+    .exec(loginStep.pause(loginPause))
+    .exec(Canvas.loadWorkpad(appConfig.baseUrl, defaultHeaders))
+
+  setUp(
+    scnDiscover
+      .inject(atOnceUsers(120), nothingFor(scnPause))
+      .andThen(
+        scnDashboard
+          .inject(atOnceUsers(120), nothingFor(scnPause))
+          .andThen(scnCanvas.inject(atOnceUsers(120)))
+      )
+  ).protocols(httpProtocol).maxDuration(simulationTimeout)
+
+}
