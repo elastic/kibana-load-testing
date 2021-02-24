4751f3c8b6d43c8b9d0beef1d4a9c135b9c1d9ad
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-02-24 20:09:58 +0300

    disable maven download progress

--- scripts/ingest_results.sh
+++ scripts/ingest_results.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd kibana-load-testing || exit
-mvn exec:java -Dexec.mainClass=org.kibanaLoadTest.ingest.Main -Dexec.classpathScope=test -Dexec.cleanupDaemonThreads=false
+mvn -ntp exec:java -Dexec.mainClass=org.kibanaLoadTest.ingest.Main -Dexec.classpathScope=test -Dexec.cleanupDaemonThreads=false
