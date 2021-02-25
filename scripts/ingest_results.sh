1c0100a8e18cf24212a829e3c17007a95efab62b
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-02-25 14:23:32 +0300

    fix scripts

--- scripts/ingest_results.sh
+++ scripts/ingest_results.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cd kibana-load-testing || exit
-mvn -ntp exec:java -Dexec.mainClass=org.kibanaLoadTest.ingest.Main -Dexec.classpathScope=test -Dexec.cleanupDaemonThreads=false
+mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -B \
+  exec:java -Dexec.mainClass=org.kibanaLoadTest.ingest.Main \
+  -Dexec.classpathScope=test -Dexec.cleanupDaemonThreads=false
