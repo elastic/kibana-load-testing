e796d0661bd841f50fa6b538dfc4cfc764d35022
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-04-02 20:45:25 +0200

    update scenario

--- src/test/scala/org/kibanaLoadTest/scenario/Dashboard.scala
+++ src/test/scala/org/kibanaLoadTest/scenario/Dashboard.scala
@@ -5,9 +5,43 @@ import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
 
 object Dashboard {
-  def load(baseUrl: String, headers: Map[String, String]): ChainBuilder =
+  def load(baseUrl: String, headers: Map[String, String]): ChainBuilder = {
     exec(
-      http("query indexPattern")
+      http("query dashboard list")
+        .get("/api/saved_objects/_find")
+        .queryParam("default_search_operator", "AND")
+        .queryParam("has_reference", "%5B%5D")
+        .queryParam("page", "1")
+        .queryParam("per_page", "1000")
+        .queryParam("search_fields", "title%5E3")
+        .queryParam("search_fields", "description")
+        .queryParam("type", "dashboard")
+        .headers(headers)
+        .header("Referer", baseUrl + "/app/dashboards")
+        .check(status.is(200))
+        .check(
+          jsonPath("$.saved_objects[?(@.type=='dashboard')].id")
+            .saveAs("dashboardId")
+        )
+        .check(jsonPath("$.saved_objects.[?(@.type=='visualization')]")
+          .findAll
+          .transform(_.map(_.replaceAll("\"name(.+?),", "")))
+          .saveAs("vizVector")
+        )
+        .check(jsonPath("$.saved_objects.[?(@.type=='map')]")
+          .findAll
+          .transform(_.map(_.replaceAll("\"name(.+?),", "")))
+          .saveAs("mapVector")
+        )
+        .check(
+          jsonPath("$.saved_objects.[?(@.type=='search')]")
+            .findAll
+            .transform(_.map(_.replaceAll("\"name(.+?),", "")))
+            .saveAs("searchVector")
+        )
+    )
+    .exec(
+      http("query indexPatterns list")
         .get("/api/saved_objects/_find")
         .queryParam("fields", "title")
         .queryParam("per_page", "10000")
@@ -20,56 +54,12 @@ object Dashboard {
             .saveAs("indexPatternId")
         )
     ).exitBlockOnFail {
-      exec(
-        http("query dashboard list")
-          .get("/api/saved_objects/_find")
-          .queryParam("default_search_operator", "AND")
-          .queryParam("page", "1")
-          .queryParam("per_page", "1000")
-          .queryParam("search_fields", "title%5E3")
-          .queryParam("search_fields", "description")
-          .queryParam("type", "dashboard")
-          .headers(headers)
-          .header("Referer", baseUrl + "/app/dashboards")
-          .check(jsonPath("$.saved_objects[:1].id").saveAs("dashboardId"))
-          .check(status.is(200))
-      ).pause(2)
-        .exec(
+        exec(
           http("query panels list")
             .post("/api/saved_objects/_bulk_get")
-            .body(
-              StringBody(
-                """
-          [
-            {
-              "id":"${dashboardId}",
-              "type":"dashboard"
-            }
-          ]
-        """
-              )
-            )
-            .asJson
+            .body(StringBody("[{\"id\":\"${dashboardId}\",\"type\":\"dashboard\"}]"))
             .headers(headers)
             .header("Referer", baseUrl + "/app/dashboards")
-            .check(
-              jsonPath(
-                "$.saved_objects[0].references[?(@.type=='visualization')]"
-              ).findAll
-                .transform(
-                  _.map(_.replaceAll("\"name(.+?),", ""))
-                ) //remove name attribute
-                .saveAs("vizVector")
-            )
-            .check(
-              jsonPath(
-                "$.saved_objects[0].references[?(@.type=='map' || @.type=='search')]"
-              ).findAll
-                .transform(
-                  _.map(_.replaceAll("\"name(.+?),", ""))
-                ) //remove name attribute
-                .saveAs("searchAndMapVector")
-            )
             .check(status.is(200))
         )
         .exec(session =>
@@ -79,13 +69,20 @@ object Dashboard {
             session("vizVector").as[Seq[String]].mkString(",")
           )
         )
-        .exec(session => {
+        .exec(session =>
           //convert Vector -> String
           session.set(
-            "searchAndMapString",
-            session("searchAndMapVector").as[Seq[String]].mkString(",")
+            "mapListString",
+            session("mapVector").as[Seq[String]].mkString(",")
           )
-        })
+        )
+          .exec(session =>
+            //convert Vector -> String
+            session.set(
+              "searchListString",
+              session("searchVector").as[Seq[String]].mkString(",")
+            )
+          )
         .exec(
           http("query visualizations")
             .post("/api/saved_objects/_bulk_get")
@@ -103,16 +100,70 @@ object Dashboard {
             .header("Referer", baseUrl + "/app/dashboards")
             .check(status.is(200))
         )
-        .exec(
-          http("query search & map")
-            .post("/api/saved_objects/_bulk_get")
-            .body(StringBody("""[ ${searchAndMapString} ]""".stripMargin))
-            .asJson
+          .exec(
+            http("query map")
+              .post("/api/saved_objects/_bulk_get")
+              .body(
+                StringBody(
+                  "[" +
+                    "$mapListString}"
+                      .concat(
+                        ", { \"id\":\"${indexPatternId}\", \"type\":\"index-pattern\"  }]"
+                      )
+                )
+              )
+              .asJson
+              .headers(headers)
+              .header("Referer", baseUrl + "/app/dashboards")
+              .check(status.is(200))
+          )
+          .exec(
+            http("query search")
+              .post("/api/saved_objects/_bulk_get")
+              .body(
+                StringBody(
+                  "[" +
+                    "$searchListString}"
+                      .concat(
+                        ", { \"id\":\"${indexPatternId}\", \"type\":\"index-pattern\"  }]"
+                      )
+                )
+              )
+              .asJson
+              .headers(headers)
+              .header("Referer", baseUrl + "/app/dashboards")
+              .check(status.is(200))
+          )
+          .exec(http("query eCommerce fields for wildcard")
+            .get("/api/index_patterns/_fields_for_wildcard")
+            .queryParam("pattern", "kibana_sample_data_ecommerce")
+            .queryParam("meta_fields", "_source")
+            .queryParam("meta_fields", "_id")
+            .queryParam("meta_fields", "_type")
+            .queryParam("meta_fields", "_index")
+            .queryParam("meta_fields", "_score")
             .headers(headers)
             .header("Referer", baseUrl + "/app/dashboards")
             .check(status.is(200))
-        )
-        .exec(
+          )
+          .exec(http("query eCommerce index pattern")
+            .get("/api/saved_objects/_find")
+            .queryParam("fields", "title")
+            .queryParam("per_page", "10")
+            .queryParam("search", "kibana_sample_data_ecommerce")
+            .queryParam("search_fields", "title")
+            .queryParam("type", "index-pattern")
+            .headers(headers)
+            .header("Referer", baseUrl + "/app/dashboards")
+            .check(status.is(200))
+          )
+          .exec(http("query input control settings")
+            .get("/api/input_control_vis/settings")
+            .headers(headers)
+            .header("Referer", baseUrl + "/app/dashboards")
+            .check(status.is(200))
+          )
+          .exec(
           http("query timeseries data")
             .post("/api/metrics/vis/data")
             .body(ElFileBody("data/timeSeriesPayload.json"))
@@ -131,4 +182,5 @@ object Dashboard {
             .check(status.is(200))
         )
     }
+  }
 }
