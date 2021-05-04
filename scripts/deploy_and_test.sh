b5748887e3accff3f294964c16f9ad9ddaa2bb05
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-05-04 12:41:12 +0200

    support multiple deployments

--- scripts/deploy_and_test.sh
+++ scripts/deploy_and_test.sh
@@ -20,29 +20,37 @@ echo "##### Install dependencies #####"
 mvn --no-transfer-progress -Dmaven.wagon.http.retryHandler.count=3 -q install -DskipTests
 echo "##### Compile source code #####"
 mvn --no-transfer-progress scala:testCompile
-echo "##### Create a new Elastic Stack deployment #####"
-mvn --no-transfer-progress exec:java -Dexec.mainClass=org.kibanaLoadTest.deploy.Create \
-  -Dexec.classpathScope=test -Dexec.cleanupDaemonThreads=false \
-  -DcloudStackVersion="${stackVersion}" \
-  -DdeploymentConfig="${deployConfig}"
-source target/cloudDeployment.txt
 
-echo "##### Running tests against Kibana cloud instance ${deploymentId} #####"
+IFS=',' read -ra version_array <<< "${stackVersion}"
 IFS=',' read -ra sim_array <<< "${simulation}"
-for i in $(seq "$test_runs_number"); do
-  for j in "${sim_array[@]}"; do
-    echo "Running simulation $j $i-time..."
-    mvn gatling:test -q -DdeploymentId="${deploymentId}" -Dgatling.simulationClass=org.kibanaLoadTest.simulation.$j
-    # wait a minute between scenarios
-    sleep 1m
+
+for version in "${version_array[@]}"; do
+  echo "##### Create a new ${version} Elastic Stack deployment #####"
+  mvn --no-transfer-progress exec:java -Dexec.mainClass=org.kibanaLoadTest.deploy.Create \
+    -Dexec.classpathScope=test -Dexec.cleanupDaemonThreads=false \
+    -DcloudStackVersion="${version}" \
+    -DdeploymentConfig="${deployConfig}"
+  # check deployment file and apply env variables
+  if [ ! -f target/cloudDeployment.txt ]; then
+	  echo "!!!Deployment failed!!!"
+	  continue
+  fi
+  source target/cloudDeployment.txt
+  echo "##### Running tests against Kibana cloud instance ${deploymentId} #####"
+  for i in $(seq "$test_runs_number"); do
+    for j in "${sim_array[@]}"; do
+      echo "Running simulation $j $i-time..."
+      mvn gatling:test -q -DdeploymentId="${deploymentId}" -Dgatling.simulationClass=org.kibanaLoadTest.simulation.$j
+      # wait a minute between scenarios
+      sleep 1m
+    done
   done
+
+  echo "##### Delete deployment ${deploymentId} #####"
+  mvn --no-transfer-progress exec:java -Dexec.mainClass=org.kibanaLoadTest.deploy.Delete \
+    -Dexec.classpathScope=test -Dexec.cleanupDaemonThreads=false \
+    -DdeploymentId="${deploymentId}"
 done
 
-echo "##### Delete deployment ${deploymentId} #####"
-mvn --no-transfer-progress exec:java -Dexec.mainClass=org.kibanaLoadTest.deploy.Delete \
-  -Dexec.classpathScope=test -Dexec.cleanupDaemonThreads=false \
-  -DdeploymentId="${deploymentId}" || exit
-
 echo "##### Zip test report #####"
-cd ..
-tar -czf report.tar.gz kibana-load-testing/target/gatling/**/*
+cd .. && tar -czf report.tar.gz kibana-load-testing/target/gatling/**/*
