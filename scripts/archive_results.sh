f4fa56d12ffd622ecb739a1b2c13c3b87ba05c1e
Dmitry Lemeshko <dzmitry.lemechko@elastic.co>
2021-07-13 14:17:28 +0200

    update commands

--- scripts/archive_results.sh
+++ scripts/archive_results.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 SCRIPT_OUTPUT_DIR="kibana-load-testing/puppeteer/output/"
-if [ -d "$PUPPETEER_DIR" ]; then
-  # Take action if $PUPPETEER_DIR exists. #
+if [ -d "$SCRIPT_OUTPUT_DIR" ]; then
   echo "archive puppeteer output"
-  tar -czf puppeteer-report.tar.gz kibana-load-testing/puppeteer/output/**/*
+  tar -czf puppeteer-report.tar.gz "$SCRIPT_OUTPUT_DIR/**/*"
 fi
 
 GATLING_OUTPUT_DIR="kibana-load-testing/target/gatling/"
 if [ -d "$GATLING_OUTPUT_DIR" ]; then
-  # Take action if $$GATLING_OUTPUT_DIR exists. #
   echo "archive gatling test results"
-  tar -czf report.tar.gz kibana-load-testing/target/gatling/**/*
+  tar -czf report.tar.gz "$GATLING_OUTPUT_DIR/**/*"
 fi
