057c95d5b3e2a047b24fe98797d871259cd51c04
Dmitry Lemeshko <dzmitry.lemechko@elastic.co>
2021-07-13 15:50:32 +0200

    use path without var

--- scripts/archive_results.sh
+++ scripts/archive_results.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SCRIPT_OUTPUT_DIR="kibana-load-testing/puppeteer/output"
-if [ -d "$SCRIPT_OUTPUT_DIR/" ]; then
+SCRIPT_OUTPUT_DIR="kibana-load-testing/puppeteer/output/"
+if [ -d "$SCRIPT_OUTPUT_DIR" ]; then
   echo "archive puppeteer output"
-  tar -czf puppeteer-report.tar.gz "$SCRIPT_OUTPUT_DIR/**/*"
+  tar -czf puppeteer-report.tar.gz kibana-load-testing/puppeteer/output/**/*
 fi
 
-GATLING_OUTPUT_DIR="kibana-load-testing/target/gatling"
-if [ -d "$GATLING_OUTPUT_DIR/" ]; then
+GATLING_OUTPUT_DIR="kibana-load-testing/target/gatling/"
+if [ -d "$GATLING_OUTPUT_DIR" ]; then
   echo "archive gatling test results"
-  tar -czf report.tar.gz "$GATLING_OUTPUT_DIR/**/*"
+  tar -czf report.tar.gz kibana-load-testing/target/gatling/**/*
 fi
