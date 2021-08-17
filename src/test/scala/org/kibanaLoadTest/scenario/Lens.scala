d697d04c688dcf556a5c43b6499803d029110c21
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-08-17 13:29:17 +0200

    [branch] replace vega simulation with lens one

---
+++ src/test/scala/org/kibanaLoadTest/scenario/Lens.scala
@@ -0,0 +1,71 @@
+package org.kibanaLoadTest.scenario
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.core.structure.ChainBuilder
+import org.kibanaLoadTest.helpers.Helper
+
+import java.util.Calendar
+
+object Lens {
+  def load(
+      id: String,
+      baseUrl: String,
+      headers: Map[String, String]
+  ): ChainBuilder = {
+    exec(session =>
+      session
+        .set("vizId", id)
+        .set("startTime", Helper.getDate(Calendar.DAY_OF_MONTH, -7))
+        .set("endTime", Helper.getDate(Calendar.DAY_OF_MONTH, 0))
+        .set("preference", System.currentTimeMillis())
+        .set("sessionId", Helper.generateUUID)
+    ).exec(
+        http("saved_objects/_bulk_get")
+          .post("/api/saved_objects/_bulk_get")
+          .headers(headers)
+          .header("Referer", baseUrl + "/app/lens")
+          .body(
+            StringBody("[{\"id\":\"${vizId}\",\"type\":\"lens\"}]")
+          )
+          .check(status.is(200))
+          .check(
+            jsonPath(
+              "$.saved_objects[?(@.id=='${vizId}')].references[?(@.name=='indexpattern-datasource-current-indexpattern')].id"
+            ).find
+              .saveAs("indexPatternId")
+          )
+      )
+      .pause(1)
+      .exec(
+        http("query index pattern meta fields")
+          .get("/api/index_patterns/_fields_for_wildcard")
+          .queryParam("pattern", "kibana_sample_data_ecommerce")
+          .queryParam("meta_fields", "_source")
+          .queryParam("meta_fields", "_id")
+          .queryParam("meta_fields", "_type")
+          .queryParam("meta_fields", "_index")
+          .queryParam("meta_fields", "_score")
+          .headers(headers)
+          .header("Referer", baseUrl + "/app/lens")
+          .check(status.is(200))
+      )
+      .exec(
+        http("lens/existing_fields")
+          .post("/api/lens/existing_fields/${indexPatternId}")
+          .headers(headers)
+          .header("Referer", baseUrl + "/app/lens")
+          .body(ElFileBody(s"data/lens/$id/existing_fields.json"))
+          .check(status.is(200))
+      )
+      .exec(
+        http("bsearch")
+          .post("/internal/bsearch")
+          .headers(headers)
+          .header("Referer", baseUrl + "/app/lens")
+          .body(ElFileBody(s"data/lens/$id/bsearch.json"))
+          .asJson
+          .check(status.is(200))
+      )
+  }
+}
