500d3a8f828b85915cacb87a81f152596538eb63
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-09-01 11:26:45 +0200

    [ci] change cloud pipeline

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -13,9 +13,3 @@ echo "Prepare environment"
 ./src/dev/ci_setup/setup.sh
 echo "Build Kibana and run load scenario"
 ./test/scripts/jenkins_build_load_testing.sh -s "${simulation}"
-
-cd ..
-# zip puppeteer report
-tar -czf puppeteer-report.tar.gz kibana-load-testing/puppeteer/output/**/*
-# zip test report
-tar -czf report.tar.gz kibana-load-testing/target/gatling/**/*
