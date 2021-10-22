ad0d561a601f0552f60dcdf6b012d240fb058658
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-10-22 02:06:35 +0200

    [puppeteer] properly wait for http requests to be completed (#187)
    * [puppeteer] wait for http responses, more scenarios
    
    * update baseline
    
    * [puppeteer] add _bulk_resolve to repeatable calls

--- puppeteer/src/puppeteer/runner.ts
+++ puppeteer/src/puppeteer/runner.ts
@@ -12,56 +12,85 @@ export async function runner(scenarioFiles: string[], options: Config) {
     if (typeof options.headless !== 'undefined' && options.headless === false) {
         browserArgs['headless'] = false;
     }
-    console.log(`Starting puppeteer: ${JSON.stringify(browserArgs)}`);
-    const browser = await puppeteer.launch(browserArgs);
     for (let i = 0; i < scenarioFiles.length; i++) {
+        console.log(`Starting puppeteer: ${JSON.stringify(browserArgs)}`);
+        const browser = await puppeteer.launch(browserArgs);
         const frameRequests = new Map<string, Request>();
         const page = await browser.newPage();
         const client = await page.target().createCDPSession();
 
         page.on('response', async(response) => {
-            const requestEvent = response.request();
-            const url = requestEvent.url();
-            if (url.startsWith(options.baseUrl) && isNotResource(url) && frameRequests.has(requestEvent._requestId)) {
-                let request = frameRequests.get(requestEvent._requestId);
-                const contentType = requestEvent?.headers()['content-type'];
-                if (request && contentType && contentType.indexOf('json') > -1) {
-                    const text = await response.text();
-                    if (isJSONString(text)) {
-                        request.responseBody = text;
+            if (response) {
+                const status = response?.status();
+                if (status && (status >= 300) && (status <=399)) {
+                    // console.log(`Redirect from ${response?.url()} to ${response?.headers()?.location}`)
+                } else {
+                    let requestEvent: puppeteer.HTTPRequest | undefined;
+                    try {
+                        requestEvent = response?.request();
+                    } catch (error) {
+                        console.error(`onResponse: failed to get HTTPRequest event`)
+                    }
+                    if (requestEvent) {
+                        let requestId: string | undefined;
+                        try {
+                            requestId = requestEvent?._requestId
+                        } catch (error) {
+                            console.error(`onResponse: failed to get requestId from HTTPRequest event`)
+                        }
+                        if (requestId && frameRequests.has(requestId)) {
+                            let request = frameRequests.get(requestId);
+                            if (request && requestEvent?.headers()?.['content-type']?.indexOf('json') > -1) {
+                                try {
+                                    const text = await response?.text();
+                                    if (isJSONString(text)) {
+                                        request.responseBody = text;
+                                    }
+                                } catch(error) {
+                                    console.error(`onResponse: failed to get response.text() from HTTPRequest event`)
+                                }
+                                
+                            }
+                        }
                     }
                 }
             }
         })
 
         client.on('Network.requestWillBeSent', (event: RequestWillBeSentEvent) => {
-            const url = event.request['url'] as string
-            if (url.startsWith(options.baseUrl) && isNotResource(url)) {
-                //console.log(`${event.requestId} ${url}`)
-                const request: Request = {
-                    frameId: event.frameId,
-                    loaderId: event.loaderId,
-                    method: event.request['method'] as string,
-                    requestUrl: event.request['url'] as string,
-                    requestHeaders: event.request['headers'] as Record<string, any>,
+            const url = event?.request?.url as string
+            try {
+                const requestId = event.requestId || ''
+                if (requestId.length > 0 && url.startsWith(options.baseUrl) && isNotResource(url)) {
+                    //console.log(`requestWillBeSent: ${event.requestId} ${url}`)
+                    const request: Request = {
+                        frameId: event?.frameId,
+                        loaderId: event?.loaderId,
+                        method: event?.request?.method as string,
+                        requestUrl: event?.request?.url as string,
+                        requestHeaders: event?.request?.headers as Record<string, any>,
+                    }
+                    if (event?.request?.hasPostData == true) {
+                        request.postData = event?.request?.postData as string
+                    }
+                    frameRequests.set(requestId, request)
                 }
-                if (event.request['hasPostData'] == true) {
-                    request.postData = event.request['postData'] as string
-                }
-                frameRequests.set(event.requestId, request)
+            } catch(error) {
+                console.log(`requestWillBeSent: failed to create Request for ${url}, skipping`)
             }
         });
 
         client.on('Network.responseReceived', (event: ResponseReceivedEvent) => {
             const url = event.response['url'] as string
-            if (url.startsWith(options.baseUrl) && isNotResource(url) && frameRequests.has(event.requestId)) {
-                let request = frameRequests.get(event.requestId)
+            if (url.startsWith(options.baseUrl) && isNotResource(url) && frameRequests.has(event?.requestId)) {
+                //console.log(`responseReceived: ${event.requestId} ${url}`)
+                let request = frameRequests.get(event?.requestId)
                 if (request) {
-                    request.responseUrl = event.response['url'] as string
-                    request.responseHeaders = event.response['headers'] as Record<string, any>
-                    request.status = event.response['status'] as number
-                    request.statusText = event.response['statusText'] as string
-                    request.responseTime = event.response['responseTime'] as number
+                    request.responseUrl = event?.response?.url as string
+                    request.responseHeaders = event?.response?.headers as Record<string, any>
+                    request.status = event?.response?.status as number
+                    request.statusText = event?.response?.statusText as string
+                    request.responseTime = event?.response?.responseTime as number
                 }
             }
         });
@@ -82,9 +111,9 @@ export async function runner(scenarioFiles: string[], options: Config) {
                 throw Error();
             }
         }
+        // close browser after each scenario
+        await browser.close();
     }
-
-    await browser.close();
 
     return scenarioResponses;
 }
