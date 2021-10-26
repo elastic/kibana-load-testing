d800f4bb6fdc488cbe587613f56b42002e77b06c
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-10-26 16:05:30 +0200

    [cloud] add scenarios

---
+++ src/test/scala/org/kibanaLoadTest/simulation/cloud/LensJourney.scala
@@ -0,0 +1,41 @@
+package org.kibanaLoadTest.simulation.cloud
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import org.kibanaLoadTest.scenario.{Lens, Login}
+import org.kibanaLoadTest.simulation.BaseSimulation
+
+class LensJourney extends BaseSimulation {
+  val scenarioName = s"Lens journey ${appConfig.buildVersion}"
+
+  props.maxUsers = 600
+
+  val scn: ScenarioBuilder = scenario(scenarioName)
+    .exec(
+      Login
+        .doLogin(
+          appConfig.isSecurityEnabled,
+          appConfig.loginPayload,
+          appConfig.loginStatusCode
+        )
+        .pause(5)
+    )
+    .exec(
+      Lens
+        .load(
+          "c762b7a0-f5ea-11eb-a78e-83aac3c38a60",
+          appConfig.baseUrl,
+          defaultHeaders
+        )
+        .pause(5)
+    )
+
+  setUp(
+    scn
+      .inject(
+        constantConcurrentUsers(20) during (3 * 60), // 1
+        rampConcurrentUsers(20) to props.maxUsers during (3 * 60) // 2
+      )
+      .protocols(httpProtocol)
+  ).maxDuration(props.simulationTimeout * 2)
+}
---
+++ src/test/scala/org/kibanaLoadTest/simulation/cloud/TSVBTimeSeriesJourney.scala
@@ -0,0 +1,43 @@
+package org.kibanaLoadTest.simulation.cloud
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import org.kibanaLoadTest.scenario.{Login, Visualize}
+import org.kibanaLoadTest.simulation.BaseSimulation
+
+class TSVBTimeSeriesJourney extends BaseSimulation {
+  val scenarioName = s"TimeSeries journey ${appConfig.buildVersion}"
+
+  props.maxUsers = 600
+
+  val scn: ScenarioBuilder = scenario(scenarioName)
+    .exec(
+      Login
+        .doLogin(
+          appConfig.isSecurityEnabled,
+          appConfig.loginPayload,
+          appConfig.loginStatusCode
+        )
+        .pause(5)
+    )
+    .exec(
+      Visualize
+        .load(
+          "tsvb",
+          "45e07720-b890-11e8-a6d9-e546fe2bba5f",
+          "data/visualize/time_series_promotion_tracking.json",
+          appConfig.baseUrl,
+          defaultHeaders
+        )
+        .pause(5)
+    )
+
+  setUp(
+    scn
+      .inject(
+        constantConcurrentUsers(20) during (3 * 60), // 1
+        rampConcurrentUsers(20) to props.maxUsers during (3 * 60) // 2
+      )
+      .protocols(httpProtocol)
+  ).maxDuration(props.simulationTimeout * 2)
+}
