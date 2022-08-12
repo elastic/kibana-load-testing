e98c3f8ef81ae759b599721de8aaa70894e85fb5
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-08-12 17:24:00 +0200

    [Ingestion/Results reporting] Disable response logging (#282)
    * disable logging responses in logback
    
    * ingest only reuqest data
    
    * fix DataStreamIngestionTest
    
    * fix ingestion on CI
    
    * remove unused function
    
    * remove unused import

--- src/test/scala/org/kibanaLoadTest/simulation/BaseSimulation.scala
+++ src/test/scala/org/kibanaLoadTest/simulation/BaseSimulation.scala
@@ -4,7 +4,6 @@ import io.gatling.core.Predef._
 import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.protocol.HttpProtocolBuilder
 import org.kibanaLoadTest.KibanaConfiguration
-import org.kibanaLoadTest.helpers.Helper.moveResponseLogToResultsDir
 import org.kibanaLoadTest.helpers.{
   CloudHttpClient,
   Helper,
@@ -105,8 +104,6 @@ class BaseSimulation extends Simulation {
 
   after {
     SimulationHelper.copyRunConfigurationToReportPath()
-    // move response log from /target to gatling report folder
-    moveResponseLogToResultsDir()
     if (
       appConfig.deploymentId.isDefined && appConfig.deleteDeploymentOnFinish
     ) {
