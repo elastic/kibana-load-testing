c0e1fe4880a2004685b9dc7ca4f075e864eefb1c
Rudolf Meijering <skaapgif@gmail.com>
2022-10-04 12:38:20 +0200

    Use correct order for Accept-Encoding header

--- src/test/scala/org/kibanaLoadTest/helpers/HttpHelper.scala
+++ src/test/scala/org/kibanaLoadTest/helpers/HttpHelper.scala
@@ -151,7 +151,7 @@ class HttpHelper(config: KibanaConfiguration) {
       .acceptHeader(
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
       )
-      .acceptEncodingHeader("br, gzip, deflate")
+      .acceptEncodingHeader("gzip, deflate, br")
       .acceptLanguageHeader("en-GB,en-US;q=0.9,en;q=0.8")
       .userAgentHeader(
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
