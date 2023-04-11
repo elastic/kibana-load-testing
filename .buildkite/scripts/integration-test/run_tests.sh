7712b70d93a1c582d68c6e7b9528e54b1b451fad
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-04-11 18:14:53 +0200

    install sdk before gsutil

--- .buildkite/scripts/integration-test/run_tests.sh
+++ .buildkite/scripts/integration-test/run_tests.sh
@@ -5,5 +5,9 @@ GCS_ARTIFACTS_DIR="${WORKSPACE}/${GCS_ARTIFACTS_REL}"
 GCS_BUCKET="gs://kibana-performance/scalability-tests"
 BUILD_HASH="0186d545-4947-44a7-a508-a25e87275d17"
 
+echo "Install gc sdk"
+curl -sSL https://sdk.cloud.google.com | bash
+
 mkdir -p "${GCS_ARTIFACTS_DIR}"
+echo "Download Kibana archive from gcs"
 gsutil cp -r "$GCS_BUCKET/$BUILD_HASH" "${GCS_ARTIFACTS_DIR}/"
