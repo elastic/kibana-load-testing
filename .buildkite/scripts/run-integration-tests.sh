5fbfab700cd58410a039e9e88a5a53eb7e24f734
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-04-21 13:24:45 +0200

    update key path

--- .buildkite/scripts/run-integration-tests.sh
+++ .buildkite/scripts/run-integration-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export API_KEY="$(vault read -field=value secret/kibana-issues/dev/cloud-staging-api-key)"
+export API_KEY="$(vault read -field=staging ci-shared/appex-qa/elastic_cloud/api_keys)"
 mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dmockserver.logLevel=WARN '-Dtest=org.kibanaLoadTest.test.integration.ElasticCloudTest' clean test
