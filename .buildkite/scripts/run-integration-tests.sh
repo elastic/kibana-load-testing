8c12140dbd861bd20751db050024c780f65b43fe
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-04-24 11:15:56 +0200

    fix reading vault key

--- .buildkite/scripts/run-integration-tests.sh
+++ .buildkite/scripts/run-integration-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export API_KEY="$(vault read -field=staging ci-shared/appex-qa/elastic_cloud/api_keys)"
+export API_KEY="$(vault kv get -field=staging kv/ci-shared/appex-qa/elastic_cloud/api_keys)"
 mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dmockserver.logLevel=WARN '-Dtest=org.kibanaLoadTest.test.integration.ElasticCloudTest' clean test
