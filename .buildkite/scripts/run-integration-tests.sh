2979ba9931370283cc13cd7d1832fd0bab2264f2
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-04-24 11:40:43 +0200

    add check if secret was read successfully

--- .buildkite/scripts/run-integration-tests.sh
+++ .buildkite/scripts/run-integration-tests.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-export API_KEY="$(vault kv get -field=staging kv/ci-shared/appex-qa/elastic_cloud/api_keys)"
-mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dmockserver.logLevel=WARN '-Dtest=org.kibanaLoadTest.test.integration.ElasticCloudTest' clean test
+API_KEY="$(vault kv get -field=staging kv/ci-shared/appex-qa/elastic_cloud/api_keys)"
+if [ -z "${API_KEY}" ]
+then
+  echo "Failed to read secrets, skipping test run"
+  exit 1
+else
+  export ${API_KEY}
+  mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Dmockserver.logLevel=WARN '-Dtest=org.kibanaLoadTest.test.integration.ElasticCloudTest' clean test
+fi
